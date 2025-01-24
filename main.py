f177a630562407cf3acd9714a716756fba88e9a2
Quicker <QuickerStudio@Gmail.com>
2025-01-25 02:07:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ import torch.multiprocessing as mp
 from train import train
 import grpc_communication
 from data_collector import DataCollector
+from ai_resource_manager import AIResourceManager
+import ctypes
+import sys
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
 if __name__ == '__main__':
+    if not is_admin():
+        ctypes.windll.shell32.ShellExecuteW(
+            None, "runas", sys.executable, __file__, None, 1
+        )
+        sys.exit()
+
     api_key = "your_api_key_here"
     kafka_servers = ["localhost:9092"]
     data_types = ["text", "image", "audio", "video", "code"]
@@ -11,6 +26,9 @@ if __name__ == '__main__':
     collector.collect_data_threaded(data_types)
     classified_data = collector.classify_data()
     collector.store_data(classified_data)
+
+    ai_resource_manager = AIResourceManager()
+    ai_resource_manager.start_monitoring()
 
     world_size = 4
     dataset = torch.utils.data.TensorDataset(torch.randn(1000, 128), torch.randn(1000, 128))
